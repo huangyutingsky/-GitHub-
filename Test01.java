5b681cd23d03bb7381ca010cf2a7839567110abd
huang_glb <a1563529792_glb@163.com>
2021-03-18 14:50:27 +0800

    第八次提交Test01

--- Test01.java
+++ Test01.java
@@ -31,7 +31,7 @@ public class Test01 extends A implements Cloneable{
 
 class A{
     String name;
-    int add;
+    int addwwww;
 
     public A(String name) {
         this.name = name;
@@ -39,6 +39,7 @@ class A{
 
     public final void method1(){
         System.out.println("A类方法一");
+        System.out.println("A类方法一addddddzzzzz");
         System.out.println("A类方法一addddddzzzzz");
     }
 }
