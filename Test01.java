3c71d89b9ffa0cf34d6679c1bd27760669f844f1
huang_glb <a1563529792_glb@163.com>
2021-03-18 15:09:38 +0800

    第十次提交Test01

--- Test01.java
+++ Test01.java
@@ -22,7 +22,9 @@ public class Test01 extends A implements Cloneable{
     public static void main(String[] args) {
         Object o = new Object();
         System.out.println(o.hashCode());
+        System.out.println("wdwdawd");
         System.out.println("...");
+        System.out.println("全都要！");
     }
 
 
