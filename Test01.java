5b83e542a4a0138b125d1ed45c126b4fd1d6c956
sky <a1563529792@163.com>
2021-03-19 15:08:35 +0800

    update Test01.java.

--- Test01.java
+++ Test01.java
@@ -23,9 +23,7 @@ public class Test01 extends A implements Cloneable{
         Object o = new Object();
         System.out.println(o.hashCode());
         System.out.println("3/20");
-        System.out.println("原始数据。。。。。。。。o");
-        System.out.println("天下第er");
-        System.out.println("合并成功");
+        System.out.println("chongxin。。。。。。。。o");
     }
 
 
